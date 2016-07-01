c7f558247fd9df884bd347f05a12c7681a7d4055
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2016-07-01 21:06:56 +0300

    Implementing dictionaryByOccurrences

--- src/main/scala/forcomp/Anagrams.scala
+++ src/main/scala/forcomp/Anagrams.scala
@@ -9,31 +9,34 @@ object Anagrams {
   /** A sentence is a `List` of words. */
   type Sentence = List[Word]
 
-  /** `Occurrences` is a `List` of pairs of characters and positive integers saying
-   *  how often the character appears.
-   *  This list is sorted alphabetically w.r.t. to the character in each pair.
-   *  All characters in the occurrence list are lowercase.
-   *
-   *  Any list of pairs of lowercase characters and their frequency which is not sorted
-   *  is **not** an occurrence list.
-   *
-   *  Note: If the frequency of some character is zero, then that character should not be
-   *  in the list.
-   */
+  /**
+    * `Occurrences` is a `List` of pairs of characters and positive integers saying
+    *  how often the character appears.
+    *  This list is sorted alphabetically w.r.t. to the character in each pair.
+    *  All characters in the occurrence list are lowercase.
+    *
+    *  Any list of pairs of lowercase characters and their frequency which is not sorted
+    *  is **not** an occurrence list.
+    *
+    *  Note: If the frequency of some character is zero, then that character should not be
+    *  in the list.
+    */
   type Occurrences = List[(Char, Int)]
 
-  /** The dictionary is simply a sequence of words.
-   *  It is predefined and obtained as a sequence using the utility method `loadDictionary`.
-   */
+  /**
+    * The dictionary is simply a sequence of words.
+    * It is predefined and obtained as a sequence using the utility method `loadDictionary`.
+    */
   val dictionary: List[Word] = loadDictionary
 
-  /** Converts the word into its character occurrence list.
-   *
-   *  Note: the uppercase and lowercase version of the character are treated as the
-   *  same character, and are represented as a lowercase character in the occurrence list.
-   *
-   *  Note: you must use `groupBy` to implement this method!
-   */
+  /**
+    * Converts the word into its character occurrence list.
+    *
+    *  Note: the uppercase and lowercase version of the character are treated as the
+    *  same character, and are represented as a lowercase character in the occurrence list.
+    *
+    *  Note: you must use `groupBy` to implement this method!
+    */
   def wordOccurrences(w: Word): Occurrences = {
     val map = w.toLowerCase.groupBy(identity).map {
       case (c: Char, r: String) => (c, r.length)
@@ -43,29 +46,33 @@ object Anagrams {
   }
 
 
-  /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = {
-    val bigWord = s.fold("") { (s, w) => s ++ w }
-    wordOccurrences(bigWord)
-  }
+  /**
+    * Converts a sentence into its character occurrence list.
+    *
+    * Folds the sentence words into one big word, and handles it with wordOccurrences.
+    */
+  def sentenceOccurrences(s: Sentence): Occurrences =
+    wordOccurrences(s.fold("") { (word, bigWord) => word ++ bigWord })
 
 
-  /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
-   *  the words that have that occurrence count.
-   *  This map serves as an easy way to obtain all the anagrams of a word given its occurrence list.
-   *
-   *  For example, the word "eat" has the following character occurrence list:
-   *
-   *     `List(('a', 1), ('e', 1), ('t', 1))`
-   *
-   *  Incidentally, so do the words "ate" and "tea".
-   *
-   *  This means that the `dictionaryByOccurrences` map will contain an entry:
-   *
-   *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
-   *
-   */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+  /**
+    * The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
+    *  the words that have that occurrence count.
+    *  This map serves as an easy way to obtain all the anagrams of a word given its occurrence list.
+    *
+    *  For example, the word "eat" has the following character occurrence list:
+    *
+    *     `List(('a', 1), ('e', 1), ('t', 1))`
+    *
+    *  Incidentally, so do the words "ate" and "tea".
+    *
+    *  This means that the `dictionaryByOccurrences` map will contain an entry:
+    *
+    *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
+    *
+    */
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] =
+    dictionary.groupBy(word => wordOccurrences(word))
 
   /** Returns all the anagrams of a given word. */
   def wordAnagrams(word: Word): List[Word] = ???
--- src/main/scala/forcomp/anagrams.sc
+++ src/main/scala/forcomp/anagrams.sc
@@ -1,3 +1,6 @@
 import forcomp.Anagrams._
 
 sentenceOccurrences(List("abcd", "e"))
+
+
+dictionaryByOccurrences.get(List(('a', 1), ('e', 1), ('t', 1))).map(_.toSet)
